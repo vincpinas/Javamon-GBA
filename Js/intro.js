4571537c931052644dffb87aa312d4864a09ff87
Vincent Pinas <ikbengerrit92@gmail.com>
2021-12-05 17:22:30 +0100

    :memo: explanation for intro functionality

--- Js/intro.js
+++ Js/intro.js
@@ -4,13 +4,14 @@ import { config } from "./var-dump.js"
 const startIntro = () => {
     const canvas = document.querySelector('.mainScreen-top')
 
-    // Create all scene elements
+    // An array of scenes used to dynamically create and destroy scenes when necessary.
     const scenes = [
         { name: 'scene1', duration: 3.2 }, 
         { name: 'scene2', duration: 2.5 }, 
         { name: 'endScene', duration: null }
     ];
 
+    // Functionality to actually create the scene elements and destroy them after the element has played for it's duration.
     scenes.map(scene => {
         let tmp = createElementClass('div', `scene ${scene.name}`);
         canvas.appendChild(tmp)
@@ -18,6 +19,7 @@ const startIntro = () => {
         destroySceneNatural(canvas, scene, tmp)
     });
 
+    // Destroys all scenes to in a way "skip" the intro.
     canvas.childNodes.length > 0 ?
         document.addEventListener('keypress', e => {
             (e.key.toLowerCase() == config.controls.accept || e.key.toLowerCase() == config.controls.cancel) ?
